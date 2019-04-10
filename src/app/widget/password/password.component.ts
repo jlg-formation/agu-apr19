02355cc10746933910f55f2d927d0bc1ee903338
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-10 17:02:41 +0200

    widget password avec icone

--- src/app/widget/password/password.component.ts
+++ src/app/widget/password/password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEye as farEye } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-password',
@@ -8,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
 export class PasswordComponent implements OnInit {
 
   inputType = 'password';
+  faEye = faEye;
+
   constructor() { }
 
   ngOnInit() {
@@ -15,9 +19,11 @@ export class PasswordComponent implements OnInit {
 
   show() {
     this.inputType = 'text';
+    this.faEye = farEye;
   }
 
   hide() {
     this.inputType = 'password';
+    this.faEye = faEye;
   }
 }
--- src/app/widget/widget.module.ts
+++ src/app/widget/widget.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordComponent } from './password/password.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [PasswordComponent],
   imports: [
-    CommonModule
+    CommonModule, FontAwesomeModule
   ],
   exports: [PasswordComponent]
 })
