11be1bb288782c119816a6806ba20e428fa87586
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-11 12:28:29 +0200

    ok

--- src/app/widget/rainbow.directive.ts
+++ src/app/widget/rainbow.directive.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef, Input } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Directive({
@@ -7,10 +7,15 @@ import { map } from 'rxjs/operators';
 })
 export class RainbowDirective {
 
+  s: Subscription;
+
   @Input() set speed(val: number) {
+    if (this.s) {
+      this.s.unsubscribe();
+    }
     const myColorObs = interval(val).pipe(map(n => n % 360));
 
-    myColorObs.subscribe(
+    this.s = myColorObs.subscribe(
       n => this.el.nativeElement.style.color = `hsl(${n}, 100%, 50%)`
     );
    }
