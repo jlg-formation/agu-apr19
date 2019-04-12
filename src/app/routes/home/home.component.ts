3dcc522a8a26dc91ae25de038089c69e1b5fa1a9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-12 10:06:52 +0200

    ok

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -12,6 +12,7 @@ export class HomeComponent implements OnInit {
     login: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required])
   });
+
   constructor() {
   }
 
