4f9714253e631e12c640d1c55a16380cebad152d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-12 14:32:15 +0200

    create

--- src/app/routes/products/products.component.ts
+++ src/app/routes/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from 'src/app/widget/rest.service';
+import { FormControl } from '@angular/forms';
 
 interface Cat {
   id: number;
@@ -15,25 +16,28 @@ interface Cat {
 })
 export class ProductsComponent implements OnInit {
 
+  name = new FormControl('titi');
+  age = new FormControl('3 mois');
+
   cats: Cat[] = [
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
-    { id: 1, name: 'Titi', age: '2 mois'},
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
+    { id: 1, name: 'Titi', age: '2 mois' },
   ];
   constructor(private rest: RestService) {
     this.rest.baseUrl = 'http://localhost:8080/ws';
-   }
+  }
 
   ngOnInit() {
     this.retrieveAllCats();
@@ -49,8 +53,13 @@ export class ProductsComponent implements OnInit {
     this.retrieveAllCats();
   }
 
+  create(): void {
+    this.rest.create('cat', { name: this.name.value, age: this.age.value })
+      .then(r => this.refresh());
+  }
+
   retrieveAllCats() {
-    this.rest.retrieveAll('cat').then( response => {
+    this.rest.retrieveAll('cat').then(response => {
       this.cats = response.content as Cat[];
     });
   }
